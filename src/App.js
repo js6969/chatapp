c3a3328d32f2c3ce73c766e9a726b38781a05aa0
drv44 <dhruvsarkar960@gmail.com>
2024-12-18 15:01:56 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/Chatspage";
 
 function App() {
   return (
     <div className="App">
-       <Route path="/" component={Homepage} />
+       <Route path="/" component={Homepage} exact />
+       <Route path="/chats" component={Chatpage} />
     </div>
   );
 }
